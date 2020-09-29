221cffae43e216c256171d67e4110a96a0f940ce
Lucas Bastos <lucas.b@adireto.com>
2020-09-29 16:23:35 -0300

    add lint and dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,72 +1,40 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 11,
-    project: 'tsconfig.json',
-    sourceType: 'module',
-  },
-  plugins: [
-    '@typescript-eslint/eslint-plugin',
-    '@typescript-eslint',
-    'eslint-plugin-import-helpers',
-  ],
   extends: [
-    'standard',
-    'plugin:@typescript-eslint/eslint-recommended',
+    "plugin:react/recommended",
+    'airbnb-typescript',
+    'airbnb/hooks',
     'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'prettier',
+    'prettier/react',
     'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
   ],
-  root: true,
+  plugins: ['react', '@typescript-eslint', 'jest'],
   env: {
-    es2020: true,
-    node: true,
+    browser: true,
+    es6: true,
     jest: true,
   },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
   rules: {
-    semi: [2, 'always', { omitLastInOneLineBlock: true }],
-    "no-unused-expressions": "true",
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    'class-methods-use-this': 'off',
-    'no-param-reassign': 'off',
-    'no-new': 'off',
-    'no-prototype-builtins': 'off',
-    'no-restricted-syntax': 'off',
-    'max-classes-per-file': 'off',
-    'no-console': 'off',
-    'import/prefer-default-export': 'off',
-    'no-useless-constructor': 'off',
-    '@typescript-eslint/naming-convention': [
+    'linebreak-style': 'off',
+    'prettier/prettier': [
       'error',
       {
-        selector: 'interface',
-        format: ['PascalCase'],
-        custom: {
-          regex: '^I[A-Z]',
-          match: true,
-        },
-      },
-    ],
-    '@typescript-eslint/explicit-module-boundary-types': ['warn'],
-    'no-underscore-dangle': 'off',
-    'import-helpers/order-imports': [
-      'warn',
-      {
-        newlinesBetween: 'always', // new line between groups
-        groups: [
-          'module',
-          '/^@server/shared/',
-          '/^@/',
-          ['parent', 'sibling', 'index'],
-        ],
-        alphabetize: {
-          order: 'asc',
-          ignoreCase: true,
-        },
+        endOfLine: 'auto',
       },
     ],
   },
